85e570c30f758e6acb2c0dd034e31dac13dafe67
Ben O'Mahony <ben@jobsasi.com>
2024-10-20 19:24:08 +0100

    Switching tree for oil and toggleterm simpler key

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,31 +1,4 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  keys = {
-    {
-      "\\",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
-      end,
-      desc = "Explorer NeoTree (Root Dir)",
-    },
-  },
-  opts = {
-    window = {
-      position = "right",
-      mappings = {
-        ["Y"] = "none",
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_by_name = {
-          ".git",
-        },
-        always_show = {
-          ".env",
-        },
-      },
-    },
-  },
+  -- disable neo-tree
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  keys = {
+    {
+      "-",
+      function()
+        require("oil").open()
+      end,
+      desc = "Open parent directory with oil",
+    },
+  },
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
