f18561d589fb179cf30a5bbb16b94ae9ec22c05d
Ben O'Mahony <ben.omahony@thoughtworks.com>
2024-10-21 09:31:39 +0100

    Updating todos

--- TODO.lua
+++ TODO.lua
@@ -1,3 +1,7 @@
+
 -- TODO: Can I create-a plugin to evaluate which plugins I use?
 -- It should evaluate the number of calls, suggest under used
 -- keymaps and finally suggest deleting
+--
+-- TODO: Add python docs telescope plugin
+-- TODO: Add telescope undo tree:
