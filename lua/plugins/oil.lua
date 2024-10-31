9a9f12c205b23b15933a92573d87919b3e43c843
benomahony <bomarni@googlemail.com>
2024-10-31 11:51:47 +0000

    Tying someone elses oil setup to try and see hidden files (.github)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,32 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
       default_file_explorer = true,
-      show_hidden = true,
-      win_options = {
-        signcolumn = "yes:2",
-        statuscolumn = "",
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
       },
-    },
-    delete_to_trash = true,
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  },
+      float = {
+        padding = 2,
+        max_width = 90,
+        max_height = 0,
+      },
+      win_options = {
+        wrap = true,
+        winblend = 0,
+      },
+      keymaps = {
+        ["<C-c>"] = false,
+        ["q"] = "actions.close",
+      },
+    })
+  end,
 }
