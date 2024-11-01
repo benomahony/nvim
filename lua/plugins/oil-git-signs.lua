e951a605c9c17e4bb1c1b7d28ff11d290d3aeb9a
benomahony <bomarni@googlemail.com>
2024-11-01 21:59:23 +0000

    Adding rich oil nvim indicators

--- lua/plugins/oil-git-signs.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- I recommend not installing this a dependency of oil as it isn't required
-  -- until you open an oil buffer
-  "FerretDetective/oil-git-signs.nvim",
-  ft = "oil",
-  opts = {},
-}
