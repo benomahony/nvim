46ecccdff3c7cea782cfd88f55cfc13387885f9a
Ben O'Mahony <ben@jobsasi.com>
2024-11-07 07:16:48 +0000

    Changing back to Tokyo night and simplifying docs setup

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,33 +1,5 @@
 return {
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function(_, opts)
-      opts.transparent = true
-      opts.italic_comments = true
-      opts.borderless_telescope = false
-      opts.transparent = true
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      transparent = true,
-      colorscheme = "cyberdream",
-    },
-  },
-
-  -- modicator (auto color line number based on vim mode)
-  {
-    "mawkler/modicator.nvim",
-    dependencies = "scottmckendry/cyberdream.nvim",
-    init = function()
-      -- These are required for Modicator to work
-      vim.o.cursorline = false
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end,
-    opts = {},
-  },
+  "folke/tokyonight.nvim",
+  lazy = true,
+  opts = { style = "moon" },
 }
