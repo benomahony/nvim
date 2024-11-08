9396b67869a9da8dd0fdaec086a90bfa6af46f06
Ben O'Mahony <ben@jobsasi.com>
2024-11-08 20:34:49 +0000

    Adding snacks.lua, switching markdown editor and adding key analyzer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,16 +1,10 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    win_options = {
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
 
-      conceallevel = {
-        default = 0,
-      },
-    },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
   },
 }
