da74abd8550258892af1504c7bb8b9e597ca91ae
Ben O'Mahony <ben@jobsasi.com>
2024-11-09 07:05:53 +0000

    Adding avante and animate, removing otter

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,8 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = { "hrsh7th/cmp-emoji", "jmbuhr/otter.nvim" },
+  dependencies = { "hrsh7th/cmp-emoji" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     table.insert(opts.sources, { name = "emoji" })
-    table.insert(opts.sources, { name = "otter" })
   end,
 }
