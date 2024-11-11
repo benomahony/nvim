a75b65d79b9faa68ae30158e3c4f513f24907a49
benomahony <bomarni@googlemail.com>
2024-11-11 13:06:32 +0000

    Changing ruff_lsp to ruff and removing useless python docs

--- lua/plugins/python-docs.lua
+++
@@ -1,9 +0,0 @@
--- TODO: Is this actually any good?
-return {
-  "syphar/python-docs.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-  event = "VeryLazy",
-  config = function()
-    require("telescope").load_extension("python_docs")
-  end,
-}
