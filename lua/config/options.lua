1b4c52416bc6ab2a964b1ad78587694edc4c4321
Ben O'Mahony <ben@jobsasi.com>
2024-11-23 20:31:36 +0000

    Adding nice inline diagnostics and fixing oil status emojis

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,16 @@ vim.opt.sidescrolloff = 10
 vim.opt.inccommand = "split"
 vim.opt.incsearch = true
 
-vim.g.snack_terminal = 'kitty'
+vim.g.snack_terminal = "kitty"
+
+vim.diagnostic.config({ virtual_text = false })
+
+-- Ensure our config persists after LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    -- Small delay to ensure this runs after the LSP's own config
+    vim.schedule(function()
+      vim.diagnostic.config({ virtual_text = false })
+    end)
+  end,
+})
