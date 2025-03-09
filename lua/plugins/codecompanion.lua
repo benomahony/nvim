29e34cefd8f8477e32b242d37324eee63181eea0
Ben O'Mahony <ben@jobsasi.com>
2025-03-09 10:55:39 +0000

    removing old plugins

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,38 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "ollama",
+      },
+      inline = {
+        adapter = "ollama",
+      },
+      command = {
+        adapter = "ollama",
+      },
+    },
+    display = {
+      chat = {
+        intro_message = "Welcome to CodeCompanion ✨! Press ? for options",
+        show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+        separator = "─", -- The separator between the different messages in the chat buffer
+        show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
+        show_settings = false, -- Show LLM settings at the top of the chat buffer?
+        show_token_count = true, -- Show the token count for each response?
+        start_in_insert_mode = true, -- Open the chat buffer in insert mode?
+      },
+      diff = {
+        enabled = true,
+        close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+        layout = "vertical", -- vertical|horizontal split for default provider
+        opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+        provider = "default", -- default|mini_diff
+      },
+    },
+  },
+}
--- lua/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "jiaoshijie/undotree",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = true,
-  position = "right",
-  keys = { -- load the plugin only when using it's keybinding:
-    { "<leader>U", "<cmd>lua require('undotree').toggle()<cr>" },
-  },
-}
