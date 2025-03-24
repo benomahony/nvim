a62c009ef2a14f48e495fbbd2d225a1bdc41dd1f
benomahony <bomarni@googlemail.com>
2025-03-24 12:19:42 +0000

    remove dashboard panels

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -37,8 +37,8 @@ return {
         sections = {
           { section = "header" },
           { section = "keys", gap = 1, padding = 1 },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           { section = "startup" },
         },
       },
