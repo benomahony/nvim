28f6152481070a554dcbfde55692e6bddfa49e15
benomahony <bomarni@googlemail.com>
2025-06-26 19:11:03 +0100

    Fix markdown and update lazy lock

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,19 +1,20 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  enabled = false,
-}, {
-  "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  -- ft = "markdown" -- If you decide to lazy-load anyway
-
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "saghen/blink.cmp",
-    "nvim-tree/nvim-web-devicons",
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
   },
-  opts = {
-    experimental = {
-      check_rtp = false,
+  {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "saghen/blink.cmp",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      experimental = {
+        check_rtp = false,
+      },
     },
   },
 }
