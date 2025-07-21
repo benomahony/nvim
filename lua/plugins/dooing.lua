924cd644e9461f4e48ccfda427e15bbebc42c70e
benomahony <bomarni@googlemail.com>
2025-07-21 20:27:18 +0100

    Great plugin cull

--- lua/plugins/dooing.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "atiladefreitas/dooing",
-  config = function()
-    require("dooing").setup({})
-  end,
-  keys = {
-    { "<leader>T", "<cmd>Dooing<cr>", desc = "Open ToDos" },
-  },
-}
