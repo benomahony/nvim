55c8d498268d84f87d0ec3bf95a5459a728d1a69
benomahony <bomarni@googlemail.com>
2025-08-01 08:36:41 +0100

    Changing suggestion colour scheme

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -7,18 +7,13 @@ return {
         clear_suggestion = "<C-]>",
         accept_word = "<C-j>",
       },
-      -- change the suggestion color to hotpink
       color = {
         -- suggestion_color = "#bb9af7",
-        suggestion_color = "#7aa2f7",
+        -- suggestion_color = "#7aa2f7",
+        suggestion_color = "#414868",
+        -- suggestion_color = "#565f89",
         cterm = 244,
       },
-      log_level = "info", -- set to "off" to disable logging completely
-      disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false, -- disables built in keymaps for more manual control
-      condition = function()
-        return false
-      end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
     })
   end,
 }
